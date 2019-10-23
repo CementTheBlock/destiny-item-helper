f2ddb419ec55523cb57196469a5581efb5217c75
CementTheBlock <cement122293@gmail.com>
2019-10-23 08:35:18 -0400

    all compiler errors related to the code I've written are fixed, still doesn't compile on my system 'because reasons'

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ extern crate flate2;
 
 use wasm_bindgen::prelude::*;
 use std::path::Path;
-use rusqlite::{params, Connection, Result, Row};
-use rusqlite::types::{Value};
+use rusqlite::{params, Connection, Result, Row, Statement};
+use rusqlite::types::Value;
 use rusqlite::types::Value::Text;
 
 //#[wasm_bindgen]
@@ -18,34 +18,47 @@ use rusqlite::types::Value::Text;
 
 //impl From<rusqlite::Error> for wasm_bindgen::JsValue {
 //    fn from(e: &rusqlite::Error) -> wasm_bindgen::JsValue {
-//        wasm_bindgen::JsValue::from_Str(e.to_str())
+//        wasm_bindgen::JsValue::from_Str(&e.to_string())
 //    }
 //}
 
 #[wasm_bindgen]
 pub fn query_db (query_string: &str, db_path: String) -> std::result::Result<String, wasm_bindgen::JsValue> {
-    let path = Path::new(std::ffi::OsString::from(db_path).as_os_str());
-    let db = Connection::open(path);
-    let query = match db {
-        Ok(db1) => db1.prepare(&*query_string),
+    let fixed_string = std::ffi::OsString::from(db_path);
+    let path = Path::new(fixed_string.as_os_str());
+    let result_db = Connection::open(path);
+    let db: Connection;
+    let result_query = match result_db {
+        Ok(d_b) => {
+            db = d_b;
+            db.prepare(&*query_string)
+        },
         Err(e) => return Err(wasm_bindgen::JsValue::from_str(&e.to_string())),
     };
-    let json_iter = match query {
-        Ok(query1) => query1.query_map(params![], to_JSON),
+    let mut query: Statement;
+    let result_iter = match result_query {
+        Ok(query_temp) => {
+            query = query_temp;
+            query.query_map(params![], to_json)
+        },
         Err(e) => return Err(wasm_bindgen::JsValue::from_str(&e.to_string())),
     };
     let mut ret_string = "{\n".to_string();
+    let json_iter = match result_iter {
+        Ok(json_iter) => json_iter,
+        Err(e) => return Err(wasm_bindgen::JsValue::from_str(&e.to_string())),
+    };
     for json in json_iter {
         match json {
-            Ok(s) => ret_string + s,
+            Ok(s) => ret_string.push_str(&s),
             Err(e) => return Err(wasm_bindgen::JsValue::from_str(&e.to_string())),
         };
-    };
+    }
     ret_string.push_str("\n}");
     Ok(ret_string)
 }
 
-fn to_JSON<'a> (row: &Row) -> Result<String> {
+fn to_json<'a> (row: &Row) -> Result<String> {
     let row_value: Value = row.get(0)?;
     match row_value {
         Text(json) => Ok(json),
